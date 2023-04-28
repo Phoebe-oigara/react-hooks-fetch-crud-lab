626c109277c0c3b22b8de5d25fe70d683ef247d0
Phoebe Oigara <phoebeoigara@gmail.com>
2023-04-28 16:18:29 +0300

    fetching data with useEffect and displaying them

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-function QuestionList() {
+function QuestionList({questions}) {
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+      {questions.map ((question) => (
+        <li key={questions.id}>{question.prompt}</li>
+      ))}
+      </ul>
     </section>
   );
 }
